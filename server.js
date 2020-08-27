0ccd1373c05b8581a85f9f620002f997cccb16f0
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-08-27 09:11:07 +0100

    server update

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(express.json());
 app.use(cors());
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
-app.use('/guests', require('./routes/guests'))
+app.use('/guests', require('./routes/guests'));
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => 
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
 
 
 const PORT = process.env.PORT || 8000;
