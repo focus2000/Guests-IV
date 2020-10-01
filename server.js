36aec9352b5f895db27ee4d2d0a29541b0e1bec5
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-10-01 17:32:59 +0100

    update

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ if(process.env.NODE_ENV === "production"){
      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     );
 }
-console.log(path.resolve(__dirname, "client", "build", "index.html"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`.blue .bold))
