f7bc5dde77e3427f45ebebfada4bb48118408f99
Kenneth Nwachukwu <nwachukwuken92@gmail.com>
2020-10-01 17:38:53 +0100

    update

---
+++ client/build/precache-manifest.743172af4f047889424aeb2d129ac687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a531ccc6ba9b958b23dbf2fbd1b625f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4771fad20c90091f92",
+    "url": "/static/css/main.56c27bfa.chunk.css"
+  },
+  {
+    "revision": "2bf580d2059e04eda474",
+    "url": "/static/js/2.a92d11d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a92d11d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4771fad20c90091f92",
+    "url": "/static/js/main.026f99bb.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
